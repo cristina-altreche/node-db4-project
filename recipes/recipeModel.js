c9ebb70525a598125015882dfca63718aeb26ffa
Cristina Altreche <c.altreche@gmail.com>
2020-08-13 16:33:48 -0400

    MVP met + stretch

--- recipes/recipeModel.js
+++ recipes/recipeModel.js
@@ -2,8 +2,18 @@ const db = require("../db-config");
 
 module.exports = {
   getRecipes,
+  getShoppingList,
+  getInstructions
 };
 
 function getRecipes() {
   return db("recipes");
+}
+
+function getShoppingList(id) {
+  return db("shopping_list").where("recipe_id", id);
+}
+
+function getInstructions(id) {
+  return db("instructions").where("recipe_id", id);
 }
--- recipes/recipeRouter.js
+++ recipes/recipeRouter.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const Recipes = require("./recipeModel");
+const { restart } = require("nodemon");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   Recipes.getRecipes()
     .then((recipes) => {
       res.status(200).json(recipes);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+router.get("/:id/shopping_list", (req, res) => {
+  Recipes.getShoppingList(req.params.id)
+    .then((shoppingList) => {
+      res.status(200).json(shoppingList);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+router.get(":id/instructions", (req, res) => {
+  Recipes.getInstructions(req.params.id)
+    .then((instructions) => {
+      res.status(200).json(instructions);
     })
     .catch((error) => {
       res.status(500).json(error);
